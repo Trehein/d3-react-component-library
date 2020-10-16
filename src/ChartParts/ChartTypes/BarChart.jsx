96d3f5b5720ae771b25668a9483cff914b9d2958
TreHein <treheinguy@gmail.com>
2020-10-16 15:45:17 -0500

    rough bar up and running

--- src/ChartParts/ChartTypes/BarChart.jsx
+++ src/ChartParts/ChartTypes/BarChart.jsx
@@ -1,19 +1,29 @@
 import React from "react"
 // import PropTypes from "prop-types"
-// import * as d3 from "d3"
+import { max, scaleBand, scaleLinear } from "d3"
 import "./styles.css"
 import Chart from "../ChartFrame/Chart"
 import { useChartDimensions } from "../ChartFrame/utils";
 
+const goalsAccessor = d => d.goals
+const keyAccessor = (d, i) => i
+
+
 const BarChart = ({ data }) => {
     const [ref, dimensions] = useChartDimensions()
+
+    const yScale = scaleBand()
+        .domain(data.map(d => d.abbr))
+        .range([0, dimensions.height])
+
+    const xScale = scaleLinear()
+        .domain([0, max(data, goalsAccessor)]) // accessor is for the metric
+        .range([0, dimensions.width])
 
     return (
         <div className="BarChart" ref={ref}>
             <Chart dimensions={dimensions}>
-                <g>
-
-                </g>
+                {data.map((d, i) => <rect x={0} y={yScale(d.abbr)} width={xScale(goalsAccessor(d))} height={yScale.bandwidth()} key={keyAccessor(d, i)} />)}
             </Chart>
         </div>
     )
