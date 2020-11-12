d0569182ce3b7a2933265510829f87d9183df24c
TreHein <treheinguy@gmail.com>
2020-11-12 14:10:02 -0600

    refactored axis

--- src/ChartParts/ChartTypes/BarChart.jsx
+++ src/ChartParts/ChartTypes/BarChart.jsx
@@ -4,25 +4,48 @@ import { max, scaleBand, scaleLinear } from "d3"
 import "./styles.css"
 import Chart from "../ChartFrame/Chart"
 import { useChartDimensions } from "../ChartFrame/utils";
+import AxisBottom from "../Parts/AxisBottom";
+import AxisLeft from "../Parts/AxisLeft";
 
 const goalsAccessor = d => d.goals
 const keyAccessor = (d, i) => i
 
 const BarChart = ({ data }) => {
     const [ref, dimensions] = useChartDimensions()
+    const innerHeight = dimensions.height - dimensions.marginTop - dimensions.marginBottom
+    const innerWidth = dimensions.width - dimensions.marginLeft - dimensions.marginRight
 
     const yScale = scaleBand()
         .domain(data.map(d => d.abbr))
-        .range([0, dimensions.height - dimensions.marginTop - dimensions.marginBottom])
+        .range([0, innerHeight])
 
     const xScale = scaleLinear()
         .domain([0, max(data, goalsAccessor)]) // accessor is for the metric
-        .range([0, dimensions.width - dimensions.marginLeft - dimensions.marginRight])
+        .range([0, innerWidth])
+
+    console.log(xScale.ticks())
 
     return (
         <div className="BarChart" ref={ref}>
             <Chart dimensions={dimensions}> 
-                {data.map((d, i) => <rect x={0} y={yScale(d.abbr)} width={xScale(goalsAccessor(d))} height={yScale.bandwidth()} key={keyAccessor(d, i)} />)}
+                <AxisBottom 
+                    xScale={xScale}
+                    innerHeight={innerHeight}
+                />
+
+                <AxisLeft 
+                    yScale={yScale}
+                />
+
+                {data.map((d, i) => 
+                    <rect 
+                        x={0} 
+                        y={yScale(d.abbr)} 
+                        width={xScale(goalsAccessor(d))} 
+                        height={yScale.bandwidth()} 
+                        key={keyAccessor(d, i)} 
+                    />
+                )}
             </Chart>
         </div>
     )
